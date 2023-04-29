95534d01c0fc316c6fa91f92456caa13c9e016bd
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-29 07:05:03 -0700

    register and login setup

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null)
@@ -7,10 +7,26 @@ export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
-    const user = null
+    const [user, setUser] = useState(null);
+    // const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        // setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        // setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
 
     const authInfo = {
         user,
+        createUser,
+        signIn,
+
+
     }
 
     return (
