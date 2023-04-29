d9c6688cd814e4b72232b9161536a17cd5eb3ec9
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-29 07:53:36 -0700

    create private route and redirect from the login page

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,16 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
+import React, { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = () => {
-    const {user} = useContext(AuthContext)
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
+
+  if (user) {
+    return children;
+  }
+  return <Navigate state={{from: location}} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
@@ -19,6 +22,6 @@ export default PrivateRoute;
  * 1. check user is logged in or not
  * 2. if user is logged in, then allow them to visit the route
  * 3. Else redirect the user to the login page
- * 4. setup the private router 
- * 5. handle loading 
-*/
+ * 4. setup the private router
+ * 5. handle loading
+ */
