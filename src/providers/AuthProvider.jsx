b8709731648a57a5595a41b62144d6646397117e
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-29 07:28:30 -0700

    observe onAuthStateChange and log out

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
+import { useEffect } from 'react';
 
 export const AuthContext = createContext(null)
 
@@ -20,13 +21,28 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+        // setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=> {
+        const unsubscribe= onAuthStateChanged(auth, loggedUser=>{
+            console.log('logged in user inside auth state observer', loggedUser)
+            setUser(loggedUser)
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    },[])
+
 
     const authInfo = {
         user,
         createUser,
         signIn,
-
-
+        logOut
     }
 
     return (
